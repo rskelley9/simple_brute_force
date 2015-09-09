f666550e0ff016dcf091e642d27b311743b49a6e
Ryan Kelley <rskelley9@gmail.com>
2015-09-09 01:54:20 -0400

    added word_list model, added minitest, and example screenshot

---
+++ models/word_list.rb
@@ -0,0 +1,26 @@
+require 'digest/md5'
+require 'gentle_brute'
+
+class WordList
+
+def initialize
+  self.wordlist = GentleBrute::BruteForcer.new
+end
+
+def self.hash_this_(phrase)
+  Digest::MD5.hexdigest(phrase)
+end
+
+def wordlist
+  self.wordlist
+end
+
+def return_phrase
+  self.word_list.next_valid_phrase
+end
+
+def regenerate!
+  self.wordlist = GentleBrute::BruteForcer.new
+end
+
+end
